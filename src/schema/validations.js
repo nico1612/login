60bdcefeaf2466c074336270342f0c752b72866f
nicolas gonzalez raggi <nicolasgonzalezraggi@MacBook-Pro-de-nicolas-3.local>
2024-05-14 21:15:52 -0300

    v002

--- src/schema/validations.js
+++ src/schema/validations.js
@@ -8,8 +8,32 @@ export const schema = yup.object().shape({
         .string()
         .matches(emailRegex, "Por favor, ingresa una dirección de correo electrónico válida.")
         .required("Por favor, ingresa tu correo electrónico."),
-    password: yup
+        password: yup
         .string()
-        .matches(passwordRegex, "La contraseña debe ser alfanumérica, con al menos una letra mayúscula, un carácter especial y mayor a 8.")
-        .required("Por favor ingrese su contraseña."),
+        .required("Por favor ingrese su contraseña.")
+        .test(
+            'min-length',
+            'La contraseña debe tener una longitud mayor o igual a 8 caracteres.',
+            value => value && value.length >= 8
+        )
+        .test(
+            'uppercase',
+            'La contraseña debe tener al menos una letra mayúscula.',
+            value => value && /[A-Z]/.test(value)
+        )
+        .test(
+            'lowercase',
+            'La contraseña debe tener al menos una letra minúscula.',
+            value => value && /[a-z]/.test(value)
+        )
+        .test(
+            'number',
+            'La contraseña debe tener al menos un número.',
+            value => value && /\d/.test(value)
+        )
+        .test(
+            'special-char',
+            'La contraseña debe tener al menos un carácter especial.',
+            value => value && /[@$!%*?&.,/()[\]{}]/.test(value)
+        ),
 });
