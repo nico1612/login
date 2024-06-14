3d8decfffb26e7aaf3752e951256a4e242ac0dcb
nicolas gonzalez raggi <nicolasgonzalezraggi@MacBook-Pro-de-nicolas-3.local>
2024-06-14 16:35:56 -0300

    fixed bugs

--- src/componentes/TaskManajer.jsx
+++ src/componentes/TaskManajer.jsx
@@ -58,9 +58,14 @@ export const TaskManager = () => {
 
     const formatDate = (dateString) => {
         const date = new Date(dateString);
-        return date.toLocaleDateString('en-US');
+        
+        const day = date.getUTCDate().toString().padStart(2, '0');
+        const month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
+        const year = date.getUTCFullYear();
+    
+        return `${day}/${month}/${year}`;
     };
-
+  
     const handleStatusChange = (status, id) => {
         setTasks(tasks.map(task => {
             let newCompletionDate = task.completed;
@@ -121,7 +126,7 @@ export const TaskManager = () => {
         dispatch(startLogout(correo))
         navigate('/')
     };
-
+console.log(tasks)
     return (
         <div className="task-manager-container">
             <div className="App">
